3e8c439710441579011d7bbc613f4af20ac79475
Hanspeter Höschle <hanspeter.hoeschle@carbonaltdelete.eu>
2023-09-13 16:11:15 +0200

    [ADD] setup.py and README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Carbon+Alt+Delete Client
+
+## Installation
+
+```bash
+pip install carbon_alt_delete @ https://github.com/CarbonAltDelete/carbonaltdelete-client
+```
+
+## Usage examples
+ Create `.env` file with your credentials or API key/secret pair or make sure the environment variables are set otherwise.
+```
+EMAIL=your_email
+PASSWORD=your_password
+SERVER=https://cad-backend-production.herokuapp.com
+```
+
+```bash
+python examples/list_all_companies.py
+```
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="Carbon+Alt+Delete Client",
+    version="2023.09.1",
+    description="Client SDK to communicate with the Carbon+Alt+Delete API",
+    url="https://github.com/CarbonAltDelete/carbonaltdelete-client",
+    author="Carbon+Alt+Delete",
+    author_email="support@carbonaltdelete.eu",
+    license="MIT",
+    packages=["carbon_alt_delete"],
+    install_requires=[
+        "pydantic[email]",
+        "python-jose",
+        "requests",
+    ],
+)
