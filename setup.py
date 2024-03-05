dbc9c709c0dad63d0d4850609e8b373f018b2369
Hanspeter Höschle <hanspeter.hoeschle@carbonaltdelete.eu>
2024-03-05 13:52:17 +0100

    [FIX] finding submodules with setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="Carbon+Alt+Delete Client",
-    version="2024.02.1",
+    version="2024.03.1",
     description="Client SDK to communicate with the Carbon+Alt+Delete API",
     url="https://github.com/CarbonAltDelete/carbonaltdelete-client",
     author="Carbon+Alt+Delete",
     author_email="support@carbonaltdelete.eu",
     license="MIT",
-    packages=["carbon_alt_delete"],
+    packages=find_packages(
+        include=[
+            "carbon_alt_delete",
+            "carbon_alt_delete.*",
+        ],
+    ),
     install_requires=[
         "pydantic[email]",
         "python-jose",
